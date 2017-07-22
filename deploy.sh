4e31417afc835bd6bbcd86230760359441c952b8
yo <turtleegg@gmail.com>
2017-07-23 02:50:16 +0800

    fix rsync path

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 date > fingerprint.txt
-rsync -av --delete --exclude-from="rsync_exclude/production.txt" /Users/yo/docker/gettoeat-docker/ yo@13.112.82.166:/home/yo/gettoeat-docker/
+rsync -av --delete --exclude-from="rsync_exclude/production.txt" /Users/yo/docker/gettoeat-docker/ yo@13.112.82.166:/gettoeat/gettoeat-docker/
